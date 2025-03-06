971b510874b840cf3c056f97efc45269bc311e9c
anubhavmt <anubhavmanvirag@gmail.com>
2025-03-07 01:00:58 +0530

    mental model one

--- src/blogData.js
+++ src/blogData.js
@@ -1,6 +1,13 @@
 // src/blogData.js
 const blogData = [
   {
+    id: 5,
+    title: "Dream of Habits, Not Goals",
+    publishedDate: "2025-03-07",
+    link: "/writing/5",
+    tags: ["life", "mental model", "2025"]
+  },
+  {
     id: 2,
     title: "Transactional Outbox Pattern In Brief",
     publishedDate: "2024-05-02",
@@ -22,12 +29,12 @@ const blogData = [
     tags: ["life", "2024"]
   },
   {
-      id: 4,
-      title: "Exploring portion of konkan coast.",
-      subheading: "Riding Solo Along the Some Konkan Coast Portion: A Journey of Freedom, Fun, and a Few Fumbles",
-      publishedDate: "2025-01-02",
-      link: "/writing/4",
-      tags: ["life","bike", "2025"]
+    id: 4,
+    title: "Exploring portion of konkan coast.",
+    subheading: "Riding Solo Along the Some Konkan Coast Portion: A Journey of Freedom, Fun, and a Few Fumbles",
+    publishedDate: "2025-01-02",
+    link: "/writing/4",
+    tags: ["life","bike", "2025"]
   },
   // Add more blogs as needed
 ];
