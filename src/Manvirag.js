232decadf985dd07c13803e02a3e306672d44b3a
anubhavmt <anubhavmanvirag@gmail.com>
2025-04-27 00:33:02 +0530

    quote

--- src/Manvirag.js
+++ src/Manvirag.js
@@ -19,6 +19,7 @@ const Home = () => {
       <h2>If you are going through hell, why would you stop in hell?. Its all about mindset. </h2>
       <h2>Believe in instinct, keep betting on yourself and move out of your comfort zone. if you don't believe in yourself, why would someone else believe in you?</h2>
       <h2>Life’s unpredictable. Don’t take it too seriously, and try to keep a balanced outlook — not too optimistic, not pessimistic. Think only on controllable things.</h2>
+      <h2>On one hand, you're wasting time scrolling and watching aimlessly. On the other, you claim you're too busy and don't have time. It's one thing to lie to others — but never cheat to yourself. </h2>
       
       
 
